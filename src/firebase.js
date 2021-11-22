9a237b53a3f76f80c5914b8c2700548e8e48eff4
Otoniel Cortez <otonielcortes1987@gmail.com>
2021-11-21 21:42:36 -0600

    formmated all the code

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,16 @@
-import firebase from "firebase";
+/** @format */
+
+import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyDIdj5trguz_ZeGqFTovmat4zUoXivI6r8",
-  authDomain: "tinder-clone-669ac.firebaseapp.com",
-  databaseURL: "https://tinder-clone-669ac.firebaseio.com",
-  projectId: "tinder-clone-669ac",
-  storageBucket: "tinder-clone-669ac.appspot.com",
-  messagingSenderId: "827942318734",
-  appId: "1:827942318734:web:f24fb516160a3c3a424fdf",
-  measurementId: "G-9N1G293TMM",
+	apiKey: 'AIzaSyDIdj5trguz_ZeGqFTovmat4zUoXivI6r8',
+	authDomain: 'tinder-clone-669ac.firebaseapp.com',
+	databaseURL: 'https://tinder-clone-669ac.firebaseio.com',
+	projectId: 'tinder-clone-669ac',
+	storageBucket: 'tinder-clone-669ac.appspot.com',
+	messagingSenderId: '827942318734',
+	appId: '1:827942318734:web:f24fb516160a3c3a424fdf',
+	measurementId: 'G-9N1G293TMM',
 });
 
 const db = firebaseApp.firestore();
